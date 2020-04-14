8bf74364c93860831d34b6922549d62251ba321f
Ljqiii <ljq917181927@gmail.com>
2020-04-14 15:05:51 +0800

    提交代码，显示执行过程，docker输出日志

--- src/views/Problem.vue
+++ src/views/Problem.vue
@@ -18,9 +18,39 @@
                     :style="{height:(problemstyleheight-(resultWindowVisable?resultWindowHeight:0)-54) + 'px'}">
                 <ProblemCodeContent :codetree="problem.initProblemCode"></ProblemCodeContent>
 
-                <div :style="{height:resultWindowHeight+'px'}" v-if="resultWindowVisable">
-                    sdafds
+                <!--                结果窗口-->
+                <div :style="{height:resultWindowHeight+'px'}" v-if="resultWindowVisable"
+                     style="display: flex;flex-direction: column;">
+
+                    <div style="height: 1px;background-color: #d8d8d8;width: 100%"></div>
+
+                    <div style="width: 100%;display: flex;flex-direction: row; flex: 1;height: 0;">
+                        <vue-custom-scrollbar style="height: 100%;width: 250px;text-align: left"
+                                              settings="maxScrollbarLength: 60">
+                            <div style="height: 15px;"></div>
+                            <el-timeline>
+                                <el-timeline-item
+                                        v-for="(activity, index) in steps"
+                                        :key="index">
+                                    {{activity.event}}
+                                </el-timeline-item>
+                            </el-timeline>
+                        </vue-custom-scrollbar>
+                        <div style="background-color:white;flex: 1;height: 100%;text-align: left;display: flex;flex-direction: column">
+                            <vue-custom-scrollbar style="height: 0;flex: 1;text-align: left;padding-left: 10px;"
+                                                  settings="maxScrollbarLength: 60">
+                                <div style="height: 10px;"></div>
+                                <div v-for="(log, index) in logs"
+                                     :key="index" style="font-family:JetBrainsMono;font-size:15px;line-height: normal ">
+                                    {{log}}
+                                </div>
+                            </vue-custom-scrollbar>
+                        </div>
+                    </div>
+
+
                 </div>
+
 
                 <div :style="{height: '53px',display: 'flex',flexDirection: 'row',justifyContent: 'space-between',alignItems: 'center',borderTop: '1px solid #E0E3EA',backgroundColor: 'white'}">
 
@@ -29,14 +59,22 @@
                         <el-button size="medium" type="text"
                                    style="margin-right: 20px;padding-left: 20px;padding-right: 11px;"
                                    v-on:click="resultWindowVisable=!resultWindowVisable">
-                            执行结果
+                            <span style="display: flex;flex-direction: row;align-items: center">
+                                <span>执行结果</span>
+                                <img :src="require('@/assets/arrowdown.svg')"
+                                     style="width: 17px;height: 17px;vertical-align: center"
+                                     v-if="resultWindowVisable==false"/>
+                                <img :src="require('@/assets/arrowup.svg')"
+                                     style="width: 17px;height: 17px;vertical-align: center"
+                                     v-if="resultWindowVisable==true"/>
+                            </span>
                         </el-button>
 
                     </div>
                     <div>
                         <el-button icon="el-icon-caret-right" size="medium" type="primary"
                                    style="margin-right: 20px;padding-left: 7px;padding-right: 11px;"
-                                   v-on:click="1">
+                                   v-on:click="submitProblem">
                             提交执行
                         </el-button>
                     </div>
@@ -56,6 +94,8 @@
     import AuthUtil from "../utils/AuthUtil";
     import ProblemCodeContent from "./problemcontet/ProblemCodeContent";
     import vueCustomScrollbar from 'vue-custom-scrollbar'
+    import ScokJs from "sockjs-client";
+    import Stomp from "stompjs";
 
     export default {
         name: 'Problem',
@@ -71,6 +111,8 @@
             window.addEventListener("resize", () => {
                 me.problemcontentheight = window.innerHeight - me.$refs.problemcontent.getBoundingClientRect().y;
             })
+            this.connectWs()
+
         },
         computed: {
             problemstyleheight() {
@@ -80,6 +122,10 @@
         },
         data() {
             return {
+                steps: [],
+                logs: [],
+                stepList: [{id: 1, content: "sdf"}, {id: 2, content: "ssdfadf"}],
+                submitids: [],
                 resultWindowVisable: false,
                 resultWindowHeight: 270,
                 problemcontentheight: 0,
@@ -89,6 +135,57 @@
             }
         },
         methods: {
+
+            connectWs: function () {
+                let that = this;
+                this.socket = new ScokJs(Url.withBase("/ws/dockerjudge"))
+                this.stompClient = Stomp.over(this.socket);
+                this.stompClient.connect({}, function (frame) {
+                    console.log(frame);
+                    that.stompClient.subscribe("/user/queue/judgestep", function (message) {
+                        that.addJudgeStep(message)
+                    })
+
+                    that.stompClient.subscribe("/user/queue/judgeresultlogs", function (message) {
+                        that.addJudgeLogs(message);
+                    })
+
+                })
+            },
+            clearResults: function () {
+                this.steps = [];
+                this.logs = [];
+            },
+            //添加step
+            addJudgeStep(message) {
+                console.log(message);
+                var body = JSON.parse(message.body);
+                this.steps.push(body);
+            },
+            //添加日志
+            addJudgeLogs(message) {
+                var body = JSON.parse(message.body);
+                var log = body.oneLineLog
+                var submitId = body.submitId
+                this.logs.push(log);
+            },
+            //提交问题
+            submitProblem() {
+                this.clearResults();
+                var that = this;
+                let data = {
+                    problemId: this.problem.id,
+                    problemCode: {problemCodeFileItems: this.problem.initProblemCode}
+                }
+                axios.post(Url.withBase("/api/judge/submit"), data
+                ).then(function (response) {
+                    that.resultWindowVisable = true;
+                    that.submitids.push(response.data.data.submitid)
+                }).catch(function (error) {
+                    console.log(error)
+                })
+            },
+
             handleTabClick(tab, event) {
                 console.log(tab, event)
             }
@@ -116,5 +213,17 @@
 </script>
 
 <style scoped lang="scss">
+    @font-face {
+        font-family: "JetBrainsMono";
+        src: url("~@/assets/font/JetBrainsMono/JetBrainsMono-Regular.woff");
+        font-weight: normal;
+        font-style: normal;
+    }
 
+</style>
+
+<style>
+    .el-timeline {
+        padding-inline-start: 30px;
+    }
 </style>
