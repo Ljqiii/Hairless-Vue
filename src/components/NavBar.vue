eb7910afad7549691ce72a24494099454f15f1d7
Ljqiii <ljq917181927@gmail.com>
2020-05-28 17:30:29 +0800

    首页

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -67,10 +67,10 @@
                         <el-dropdown-item command="tome">
                             个人主页
                         </el-dropdown-item>
-                        <el-dropdown-item command="a">黄金糕</el-dropdown-item>
-                        <el-dropdown-item command="b">狮子头</el-dropdown-item>
-                        <el-dropdown-item command="c">螺蛳粉</el-dropdown-item>
-                        <el-dropdown-item command="d" disabled>双皮奶</el-dropdown-item>
+<!--                        <el-dropdown-item command="a">黄金糕</el-dropdown-item>-->
+<!--                        <el-dropdown-item command="b">狮子头</el-dropdown-item>-->
+<!--                        <el-dropdown-item command="c">螺蛳粉</el-dropdown-item>-->
+<!--                        <el-dropdown-item command="d" disabled>双皮奶</el-dropdown-item>-->
                         <el-dropdown-item command="logout" divided icon="el">退出登录</el-dropdown-item>
                     </el-dropdown-menu>
                 </el-dropdown>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,34 @@
 <template>
     <div class="home">
         <el-main>
-            <p>{{this.$store.getters["authStore/gettestAuthTest"]}}</p>
-            <button v-on:click="changeaa">s</button>
-            <button v-on:click="sendwsdata">sendws</button>
-            <button v-on:click="connectWs">connectws</button>
+            <div style="height: 20px;"></div>
+            <el-row :gutter="20">
+                <el-col :span="8" style="min-height: 1px">
+                    <div class="grid-content bg-purple" style="margin-left: 60px">
+                        <div style="height: 50px"></div>
+                        <h1>一个全新的在线编程平台</h1>
+                        <div>Hairless让你在线练习Java编程，在线运行。帮你提高编程技能</div>
+                        <div style="height: 30px;"></div>
+                        <div>
+                            <el-button type="primary" v-on:click="showlogin">注册账号</el-button>
+                            <el-button type="primary" v-on:click="start">开始练习</el-button>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :span="12" style="min-height: 1px">
+                    <div class="" style="background-color: #f8f8f8">
+                        <img :src="require('@/assets/Docker_Java.png')"
+                             style="height: 360px;width: 696px;margin-left: 100px;margin-right: 60px"/>
+                    </div>
+                </el-col>
+            </el-row>
+
+            <div style="height: 20px"></div>
+
+            <!--                <p>{{this.$store.getters["authStore/gettestAuthTest"]}}</p>-->
+            <!--                <button v-on:click="changeaa">s</button>-->
+            <!--                <button v-on:click="sendwsdata">sendws</button>-->
+            <!--                <button v-on:click="connectWs">connectws</button>-->
         </el-main>
     </div>
 </template>
@@ -15,6 +39,8 @@
     import testjs from "./testjs";
     import Stomp from 'stompjs';
     import ScokJs from 'sockjs-client';
+    import Event from "../plugins/event";
+    import authapi from "../api/authApi";
 
     export default {
         name: 'Home',
@@ -23,6 +49,9 @@
             NavBar,
             HelloWorld
         }, computed: {
+            islogin() {
+                return this.$store.getters.getIsLogin;
+            },
             testText() {
                 return testjs.gettestText()
             }
@@ -40,6 +69,17 @@
                 text: this.$store.getters["authStore/gettestAuthTest"]
             }
         }, methods: {
+            start: function () {
+                if (this.islogin == true) {
+                    this.$router.push("/problemset")
+                } else {
+                    sessionStorage.setItem("pathBeforeLogin", "/problemset/all")
+                    authapi.login()
+                }
+            },
+            showlogin: function () {
+                Event.$emit("showauthdialog");
+            },
             connectWs: function () {
                 this.socket = new ScokJs('http://hairless.ljqiii.xyz:7082/ws/dockerjudge')
                 this.stompClient = Stomp.over(this.socket);
